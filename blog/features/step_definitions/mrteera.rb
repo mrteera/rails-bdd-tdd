8d93ccd77033308085b180adc80576a4bc59bb81
mrteera <inbox@mrteera.com>
2016-08-23 17:49:07 +0700

    Bring back cucumber steps

--- blog/features/step_definitions/mrteera.rb
+++ blog/features/step_definitions/mrteera.rb
@@ -1,11 +1,11 @@
 Given(/^Teera access welcome page$/) do
-	  pending # Write code here that turns the phrase above into concrete actions
+	  visit '/'
 end
 
 Then(/^Teera should see Find me in app text$/) do
-	  pending # Write code here that turns the phrase above into concrete actions
+	  expect(page).to have_content('Hello, Rails!')
 end
 
 Then(/^Teera should see the summation of one plus one$/) do
-	  pending # Write code here that turns the phrase above into concrete actions
+	  expect(page).to have_content('Summation: 2')
 end
